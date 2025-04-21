ff3f37bfdba49eb2e96ea0e4fa7367b1170a7040
SJ <ligerssj@naver.com>
2025-04-21 11:38:50 +0900

    quiz js

--- 07_JS/6_dom_api2.js
+++ 07_JS/6_dom_api2.js
@@ -6,7 +6,7 @@ console.log(boxEls); // 찾은 요소들의 리스트가 반환됨 NodeList()[]
 // forEach() 메소드 사용: 배열에서 각 요소에 대한 반복 처리를 수행
 // 인수(인자값)로 반복을 돌면서 꺼내온 요소를 처리하는 함수 작성
 // 처리 함수 작성 시 매개변수(현재 가져온 요소, 요소의 인덱스) 순서가 중요!!
-// @데코레이터?
+// 데코레이터?
 boxEls.forEach(function (boxEl, index) {
     console.log(index, boxEl);
 
@@ -20,4 +20,5 @@ console.log(boxEl.textContent); // 요소의 내용 출력
 
 boxEl.textContent = 'CHANGE!!';
 console.log(boxEl.textContent);
-
+// (참고) innerHTML: html 태그까지 조작하거나 삽입하고 싶을 때
+boxEl.innerHTML = '<b>Inner!!</b>';
