d58b9cd3453227f398b2bcbd5aeb95c684c08b53
SJ <ligerssj@naver.com>
2025-04-22 12:36:57 +0900

    포트폴리오 연습

--- 08_portfolio-site/js/main.js
+++ 08_portfolio-site/js/main.js
@@ -1,2 +1,15 @@
-// 스크롤 초기화 방지
 history.scrollRestoration = "auto";
+
+// ScrollMagic 사용
+const spyEls = document.querySelectorAll('section.scroll-spy')
+
+const controller = new ScrollMagic.Controller();
+spyEls.forEach(spyEl => {
+    // 객체 생성
+    new ScrollMagic.Scene({ // 감시할 장면 추가 및 옵션 지정
+        triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+        triggerHook: 0.5 // 화면에 50% 지점에서 보여짐 여부 감시(0~1사이 지정)
+        // 메소드 체이닝
+    }).setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+        .addTo(controller); // 컨트롤러에 장면을 할당(필수!) - 라이브러리에서 지정한 문법으로 깊게 이해X
+});
